4b6224efbc92b9745934811b53f40cc5fb2bd676
Scott Huang <mingwei6@illinois.edu>
2021-04-09 16:43:49 -0500

    Draft on get response for movie, actor, and director pages

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def homepage():
 
 @app.route('/movies/', methods=[GET, PUT, POST, DELETE])
 def moviepage():
-    fake_movie = [{'MovieId': 3532, 'Title': 'My Moovie', 'Runtime': 215, 'Publication_Year': 2022, 'Genre': 'Adult', 'Language': 'English'}]
+    fake_movie = [{'MovieId': 3532, 'Title': 'My Moovie', 'Runtime': 215,
+                  'Publication_Year': 2022, 'Genre': 'Adult', 'Language': 'English'}]
     movie_id = request.args.get('id', default = -1, type = int)
     if movie_id != -1:
         return render_template('movies.html', movie_infos=fake_movie)
@@ -26,25 +27,29 @@ def moviepage():
 
 @app.route('/actors/', methods=[GET, PUT, POST, DELETE])
 def actorpage():
+    fake_actor = [{'ActorId': 53314, 'Actor_Name': 'My Actor', 'Birth_Year': 2077,
+                   'Most_Known_Titles': 'Aha'}]
     actor_id = request.args.get('id', default = -1, type = int)
     if actor_id != -1:
-        return {'status': 'OK'}
+        return render_template('actors.html', actor_infos=fake_actor)
     
     recommand = request.args.get('recommand', default = False, type = bool)
     if recommand:
-        return {'status': 'OK'}
+        return render_template('actors.html', actor_infos=fake_actor+fake_actor+fake_actor)
     
     abort(404)
 
 @app.route('/directors/', methods=[GET, PUT, POST, DELETE])
 def directorpage():
+    fake_director = [{'DirectorId': 321685, 'Director_name': 'My Director', 'Birth_Year': 1344,
+                      'Award': None}]
     director_id = request.args.get('id', default = -1, type = int)
     if director_id != -1:
-        return {'status': 'OK'}
+        return render_template('directors.html', director_infos=fake_director)
     
     recommand = request.args.get('recommand', default = False, type = bool)
     if recommand:
-        return {'status': 'OK'}
+        return render_template('directors.html', director_infos=fake_director+fake_director)
     
     abort(404)
 
