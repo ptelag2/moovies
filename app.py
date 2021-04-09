389da4880e618a41c7d762819ecc0fe20c987052
Scott Huang <mingwei6@illinois.edu>
2021-04-09 16:10:43 -0500

    initial draft

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, abort, jsonify, render_template
+
+GET = 'GET'
+PUT = 'PUT'
+POST = 'POST'
+DELETE = 'DELETE'
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/movies/', methods=[GET, PUT, POST, DELETE])
+def moviepage():
+    fake_movie = [{'MovieId': 3532, 'Title': 'My Moovie', 'Runtime': 215, 'Publication_Year': 2022, 'Genre': 'Adult', 'Language': 'English'}]
+    movie_id = request.args.get('id', default = -1, type = int)
+    if movie_id != -1:
+        return render_template('movies.html', movie_infos=fake_movie)
+    
+    recommand = request.args.get('recommand', default = False, type = bool)
+    if recommand:
+        return render_template('movies.html', movie_infos=fake_movie+fake_movie+fake_movie)
+    
+    abort(404)
+
+@app.route('/actors/', methods=[GET, PUT, POST, DELETE])
+def actorpage():
+    actor_id = request.args.get('id', default = -1, type = int)
+    if actor_id != -1:
+        return {'status': 'OK'}
+    
+    recommand = request.args.get('recommand', default = False, type = bool)
+    if recommand:
+        return {'status': 'OK'}
+    
+    abort(404)
+
+@app.route('/directors/', methods=[GET, PUT, POST, DELETE])
+def directorpage():
+    director_id = request.args.get('id', default = -1, type = int)
+    if director_id != -1:
+        return {'status': 'OK'}
+    
+    recommand = request.args.get('recommand', default = False, type = bool)
+    if recommand:
+        return {'status': 'OK'}
+    
+    abort(404)
+
+@app.route('/reviews/', methods=[GET, PUT, POST, DELETE])
+def reviewpage():
+    movie_id = request.args.get('movie_id', default = -1, type = int)
+    if movie_id != -1:
+        return {'status': 'OK'}
+    
+    abort(404)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
